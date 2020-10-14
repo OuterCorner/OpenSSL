edfce992975ef2ac6456ec3a08f6c43c3b6c75af
Paulo Andrade <paulo@outercorner.com>
2020-10-14 11:57:08 +0100

    Revert "Explicitly stating this is a dynamic library"
    This reverts commit 88ef1edfe19751f063e841e46408812949ee2bd9.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "OpenSSL",
-			type: .dynamic,
             targets: ["OpenSSL"]
         )
     ],
