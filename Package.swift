88ef1edfe19751f063e841e46408812949ee2bd9
Paulo Andrade <paulo@outercorner.com>
2020-10-14 11:43:36 +0100

    Explicitly stating this is a dynamic library

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "OpenSSL",
+			type: .dynamic,
             targets: ["OpenSSL"]
         )
     ],
