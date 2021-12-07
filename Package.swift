cf2ae922de90e43a921b6de944df8c4db57282dc
Jaroslav Pulik <jaroslav.pulik@gmail.com>
2021-12-07 12:37:09 +0100

    Minimum deployment targed for iOS raised to iOS 9

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "OpenSSL",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8)
+        .macOS(.v10_10), .iOS(.v9)
         ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
