782274d2c6e609cb8b6acb6a71c57c2b5ed70af3
Paulo Andrade <paulo@outercorner.com>
2022-02-04 18:20:12 +0000

    Not importing Foundation on umbrella header so this file can more easily be included on non ObjC code.

--- OpenSSL/OpenSSL.h
+++ OpenSSL/OpenSSL.h
@@ -6,15 +6,4 @@
 // Copyright © 2018 Outer Corner. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-//! Project version number for OpenSSL.
-FOUNDATION_EXPORT double OpenSSLVersionNumber;
-
-//! Project version string for OpenSSL.
-FOUNDATION_EXPORT const unsigned char OpenSSLVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <OpenSSL/PublicHeader.h>
-
-
 
